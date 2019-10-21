897b15c58004a41ee0662bfe88c284457b78e32b
Qiao6651 <55575855+Qiao6651@users.noreply.github.com>
2019-10-21 16:57:16 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+import { CounterService } from './counter.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counters';
+  constructor(private counterService: CounterService) {}
+
+  reset() {
+    this.counterService.reset()
+  }
 }
